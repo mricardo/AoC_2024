a81bd848ca129030f27af8c1d05c343c7ff54d48
mricardo <miguelcricardo@gmail.com>
2024-12-04 18:18:52 +0000

    Day 3 Part A

---
+++ Day3/Day3_PartA.py
@@ -0,0 +1,19 @@
+import re
+
+if __name__ == "__main__":
+    filename = "input_3.txt"
+
+    pattern = "mul\((\d{1,3})\,(\d{1,3})\)"
+
+    total = 0
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+        for line in lines:
+            matches = re.findall(pattern, line)
+
+            for d1, d2 in matches:
+                total += int(d1) * int(d2)
+    
+    print(total)
+
