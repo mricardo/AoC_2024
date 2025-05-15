f5f7aecf7a3f6b28c789f1b3c20d9a58c68498d3
Miguel C. Ricardo <miguelcricardo@gmail.com>
2025-05-15 21:48:20 +0100

    Add files via upload

--- Day19/Day19_PartA.py
+++ Day19/Day19_PartA.py
@@ -1,1 +1,23 @@
+# each towel has one more more stripes: white (w), blue(b), red(r), green(g)
+# each design is a sequence of towels
+# arrange towels into the designs
 
+def check_valid_design(towels, design):
+    if not design:
+        return True
+    
+    for towel in towels:
+        if design.startswith(towel) and check_valid_design(towels, design[len(towel):]):
+            return True
+    
+    return False
+
+with open("Day19/Day19_Input.txt", "r") as file:
+    lines = [line.strip() for line in file]
+
+towels = set(lines[0].split(", "))
+designs = lines[2:]
+
+total_valid_designs = sum(check_valid_design(towels, design) for design in designs)
+
+print("Total valid designs:", total_valid_designs)
